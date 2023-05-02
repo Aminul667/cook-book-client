f975b34f9cbb7fe924063d4e23eeb81847155e06
Aminul667 <rahathssn@gmail.com>
2023-05-02 14:01:10 +0600

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,7 @@ function App() {
 
   return (
     <>
-      <h2 className="text-red-800 bg-slate-500">Hello World</h2>
-      <div>
-        <button className="btn">Button</button>
-        <button className="btn btn-primary">Button</button>
-        <button className="btn btn-secondary">Button</button>
-        <button className="btn btn-accent">Button</button>
-        <button className="btn btn-ghost">Button</button>
-        <button className="btn btn-link">Button</button>
-      </div>
+      
     </>
   );
 }
---
+++ src/components/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-[url('../../public/images/banner.jpg')] text-center bg-no-repeat bg-center lg:mx-9">
+      <div className="backdrop-brightness-50 py-28 lg:py-44">
+        <h2 className="text-4xl lg:text-6xl text-white font-medium">Enjoy Cooking</h2>
+        <p className="text-3xl lg:text-4xl font-normal text-white mt-6">
+          Easily Learn to cook from our recipes
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
